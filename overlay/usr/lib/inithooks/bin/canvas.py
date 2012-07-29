b3840e2188706aca36555b7931930e29a23959eb
Alon Swartz <alon@turnkeylinux.org>
2012-07-29 15:19:42 +0300

    Revert "removed DEFAULT_DOMAIN from inithook"
    This reverts commit 367742dec0f1c53173f92992e7381f1e7086aa08.
    
    rational:
        - We depend on DEFAULT in preseeding as different appliances require
          different defaults (www.example.com, example.com, DOMAIN)

--- overlay/usr/lib/inithooks/bin/canvas.py
+++ overlay/usr/lib/inithooks/bin/canvas.py
@@ -5,6 +5,7 @@ Option:
     --pass=     unless provided, will ask interactively
     --email=    unless provided, will ask interactively
     --domain=   unless provided, will ask interactively
+                DEFAULT=www.example.com
 """
 
 import sys
@@ -23,6 +24,8 @@ def usage(s=None):
     print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
     print >> sys.stderr, __doc__
     sys.exit(1)
+
+DEFAULT_DOMAIN="www.example.com"
 
 def main():
     try:
@@ -66,7 +69,10 @@ def main():
         domain = d.get_input(
             "Canvas Domain",
             "Enter the domain to serve Canvas.",
-            "www.example.com")
+            DEFAULT_DOMAIN)
+
+    if domain == "DEFAULT":
+        domain = DEFAULT_DOMAIN
 
     salt = "".join(random.choice(string.letters) for line in range(20))
     hash = password + salt
